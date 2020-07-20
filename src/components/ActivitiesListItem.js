cbe9d6f611b0b6f80eece5423181f4407803a889
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-20 16:13:54 +0200

    Added icon

--- src/components/ActivitiesListItem.js
+++ src/components/ActivitiesListItem.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { List } from 'react-native-paper';
 
-const ActivitiesListItem = ({ details, onPress }) => {
+const ActivitiesListItem = ({ details, onPressDetails }) => {
   return (
     <>
-      <List.Item title={details.name} onPress={() => onPress(details['_id'])} />
+      <List.Item
+        title={details.name}
+        onPress={() => onPressDetails(details['_id'])}
+        right={props => <List.Icon {...props} icon="star-box-outline" />}
+      />
     </>
   )
 };
