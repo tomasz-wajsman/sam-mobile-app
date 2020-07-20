4b94fa853f557e902770d9347542e9af112fee5e
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-20 15:52:21 +0200

    Added modal and list

--- src/screens/ActivityScreen.js
+++ src/screens/ActivityScreen.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import styles from '../styles';
 import ActivitiesList from '../components/ActivitiesList';
+import ActivityModal from '../components/modals/ActivityModal';
+
+const defaultActivities = [
+  { _id: '111111111111111111111111', name: 'Swimming', category: 'swimming', startDate: 123, endDate: 456, distance: 5000 },
+  { _id: '111111111111111111111112', name: 'Cycling in mountains', category: 'cycling', startDate: 123, endDate: 456, distance: 45000 },
+  { _id: '111111111111111111111113', name: 'Football match', category: 'football', startDate: 123, endDate: 456 },
+];
 
 const ActivityScreen = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [activities, setActivities] = useState(defaultActivities);
+  const [selectedActivityID, setSelectedActivityID] = useState('')
+
+  const findActivityIndexByID = activityID => activities.findIndex(activity => activity['_id'] === activityID);
+  const handlePressItem = activityID => {
+    setSelectedActivityID(findActivityIndexByID(activityID));
+    setModalVisible(true);
+  };
+  const handleDismissModal = () => {
+    setModalVisible(false);
+  };
+
   return (
     <View style={styles.layout.container}>
-      <ActivitiesList />
+      <ActivityModal
+        details={activities[selectedActivityID]}
+        visible={modalVisible}
+        onDismiss={handleDismissModal}
+      />
+      <ActivitiesList
+        items={activities}
+        onPressItem={handlePressItem}
+      />
     </View>
   )
 };
