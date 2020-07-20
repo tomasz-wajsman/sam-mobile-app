e71221894d4d2373e0f15d74d5cf939b9fa0d119
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-20 15:30:23 +0200

    Added activities list

--- src/screens/ActivityScreen.js
+++ src/screens/ActivityScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import styles from '../styles';
+import ActivitiesList from '../components/ActivitiesList';
 
 const ActivityScreen = () => {
   return (
-    <View style={styles.layout}>
-      <Text>Text</Text>
+    <View style={styles.layout.container}>
+      <ActivitiesList />
     </View>
   )
 };
