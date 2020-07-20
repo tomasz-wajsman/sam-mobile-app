1549ba340f1012a4e743b66032d92e14da8f4ab5
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-20 16:13:30 +0200

    Added Paper provider

--- App.js
+++ App.js
@@ -2,12 +2,13 @@
 import React from 'react';
 import { View, StatusBar } from 'react-native';
 import ActivityScreen from './src/screens/ActivityScreen';
+import { Provider } from 'react-native-paper';
 
 export default function App() {
   return (
-    <>
+    <Provider>
       <StatusBar barStyle="dark-content" backgroundColor={'#ff9900'} />
       <ActivityScreen />
-    </>
+    </Provider>
   );
 }
