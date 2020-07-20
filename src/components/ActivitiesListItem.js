171072508710d41f71d6f7dec88465c1398d138e
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-20 16:16:01 +0200

    Changed icon

--- src/components/ActivitiesListItem.js
+++ src/components/ActivitiesListItem.js
@@ -7,7 +7,7 @@ const ActivitiesListItem = ({ details, onPressDetails }) => {
       <List.Item
         title={details.name}
         onPress={() => onPressDetails(details['_id'])}
-        right={props => <List.Icon {...props} icon="star-box-outline" />}
+        right={props => <List.Icon {...props} icon="pencil" />}
       />
     </>
   )
