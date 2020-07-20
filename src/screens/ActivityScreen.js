c73d15eaa8de496df81c5f6d0df238ad85c6183d
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-20 15:08:15 +0200

    Added the screen

---
+++ src/screens/ActivityScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from '../styles';
+
+const ActivityScreen = () => {
+  return (
+    <View style={styles.layout}>
+      <Text>Text</Text>
+    </View>
+  )
+};
+export default ActivityScreen;
