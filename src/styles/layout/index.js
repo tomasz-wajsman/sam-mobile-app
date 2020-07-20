1539f44c2c1be6d9652377b9f1a972a0840fd8bc
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-20 15:52:07 +0200

    Added padding for container

--- src/styles/layout/index.js
+++ src/styles/layout/index.js
@@ -2,7 +2,8 @@ import { StyleSheet } from 'react-native';
 
 const layout = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    paddingHorizontal: 10
   }
 });
 export default layout;
