261276cf5dce888e6de558761f9beea01352d567
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-20 16:16:43 +0200

    Refactor
    Changed handler names

--- src/components/ActivitiesList.js
+++ src/components/ActivitiesList.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Card, Title, List } from 'react-native-paper';
+import { Card, Title, List, Button } from 'react-native-paper';
 import ActivitiesListItem from './ActivitiesListItem';
 
-const ActivitiesList = ({ items, onPressItem }) => {
+const ActivitiesList = ({ items, onPressAdd, onPressDetails }) => {
   return (
     <View>
       <Title>Activities</Title>
       <List.Section>
         <List.Accordion title="Activities">
           {
-            items.map(activity => <ActivitiesListItem details={activity} onPress={onPressItem} />)
+            items.map(activity => <ActivitiesListItem key={activity['_id']} details={activity} onPressDetails={onPressDetails} />)
           }
         </List.Accordion>
       </List.Section>
+      <Button onPress={onPressAdd}>Add a new entry</Button>
     </View>
   );
 };
