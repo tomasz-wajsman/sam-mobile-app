94f78ff0c74266df2ac476a83ce6beee264cb654
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-21 13:48:30 +0200

    Added validation

--- src/components/modals/ActivityEditorModal.js
+++ src/components/modals/ActivityEditorModal.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Modal, Card, Button, Paragraph, Portal, TextInput } from 'react-native-paper';
+import { Modal, Card, Button, Paragraph, Portal, TextInput, HelperText } from 'react-native-paper';
 
 import util from '../../util';
 
@@ -14,13 +14,91 @@ const ActivityEditorModal = ({ visible, editing, details, onConfirm, onDismiss }
       distance: details.distance || ''
     }
   );
+
+  const [messages, setMessages] = useState(
+    {
+      name: '',
+      category: '',
+      startDate: '',
+      endDate: '',
+      distance: ''
+    }
+  );
+  const [inputCorrect, setInputCorrect] = useState(
+    {
+      name: false,
+      category: false,
+      startDate: true,
+      endDate: true,
+      distance: true
+    }  
+  );
+
   const handleInput = (field, value) => {
     const temp = { ...input };
-    console.log(field, value);
     if (temp.hasOwnProperty(field)) {
       temp[field] = value;
+      // validate the value
+      validateInput(field, value);
       setInput(temp);
     }
+  };
+
+  const validateInput = (field, value) => {
+    const ic = {...inputCorrect};
+    const msg = {...messages};
+    switch (field) {
+      case 'name':
+        if (value === '') {
+          msg.name = 'Empty name';
+          ic.name = false;
+        } else {
+          msg.name = '';
+          ic.name = true;
+        }
+        // name
+        break;
+      case 'category':
+        // category
+        if (value === '') {
+          msg.category = 'Empty category';
+          ic.category = false;
+        } else {
+          msg.category = '';
+          ic.category = true;
+        }
+        break;
+      case 'distance':
+        // distance
+        if (value !== '') {
+          const distanceNum = Number.parseInt(value);
+          if (Number.isNaN(distanceNum)) {
+            msg.distance = 'Incorrect distance format';
+            ic.distance = false;
+          } else if (distanceNum <= 0) {
+            msg.distance = 'Negative or zeroth distance';
+            ic.distance = false;
+          } else {
+            msg.distance = '';
+            ic.distance = true;
+          }
+        }
+        break;
+    }
+    setInputCorrect(ic);
+    setMessages(msg);
+  };
+
+  const checkForm = () => {
+    let correct = true;
+    for (const [k, v] of Object.entries(inputCorrect)) {
+      if (!v) {
+        console.log(k)
+        correct = false;
+        break;
+      }
+    }
+    return correct;
   };
 
   if (visible) {
@@ -30,16 +108,52 @@ const ActivityEditorModal = ({ visible, editing, details, onConfirm, onDismiss }
           <Card>
             <Card.Title title={editing ? "Editing an activity" : "Adding an activity"} />
             <Card.Content>
-              <TextInput placeholder="Activity name" value={input.name} onChangeText={text => handleInput('name', text)} />
-              <TextInput placeholder="Category" value={input.category} onChangeText={text => handleInput('category', text)} />
-              <TextInput placeholder="Start date" value={input.startDate} onChangeText={text => handleInput('startDate', text)} />
-              <TextInput placeholder="End date" value={input.endDate} onChangeText={text => handleInput('endDate', text)} />
-              <TextInput placeholder="Distance" value={input.distance} onChangeText={text => handleInput('distance', text)} />
+              <TextInput
+                placeholder="Activity name"
+                value={input.name}
+                onChangeText={text => handleInput('name', text)}
+              />
+              <HelperText type="error" visible={messages.name !== ''}>
+                {messages.name}
+              </HelperText>
+              <TextInput
+                placeholder="Category"
+                value={input.category}
+                onChangeText={text => handleInput('category', text)}
+              />
+              <HelperText type="error" visible={messages.category !== ''}>
+                {messages.category}
+              </HelperText>
+              <TextInput
+                placeholder="Start date"
+                value={input.startDate}
+                onChangeText={text => handleInput('startDate', text)}
+              />
+              <TextInput
+                placeholder="End date"
+                value={input.endDate}
+                onChangeText={text => handleInput('endDate', text)}
+              />
+              <TextInput
+                placeholder="Distance (optional)"
+                value={input.distance}
+                onChangeText={text => handleInput('distance', text)}
+                keyboardType="numeric"
+              />
+              <HelperText type="error" visible={messages.distance !== ''}>
+                {messages.distance}
+              </HelperText>
             </Card.Content>
             {
               editing
-                ? <Button onPress={() => onConfirm(details['_id'], input)}>SAVE</Button>
-                : <Button onPress={() => onConfirm(input)}>ADD</Button>
+                ? <Button onPress={() => checkForm()
+                  ? onConfirm(details['_id'], input)
+                  : util.alert.showAlert('Error', 'Some of the inputs values are missing or incorrect. Check them and try again.')
+                }>SAVE</Button>
+                : <Button onPress={() => checkForm()
+                  ? onConfirm(input)
+                  : util.alert.showAlert('Error', 'Some of the input values are missing or incorrect. Check them and try again.')
+                }>ADD</Button>
             }
             <Button onPress={onDismiss}>HIDE</Button>
           </Card>
