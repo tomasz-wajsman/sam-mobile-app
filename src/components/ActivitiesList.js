8e41b5921a743156e16d8978fa6ab9aa13bf17e4
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-21 11:44:46 +0200

    Added a new representation of list items

--- src/components/ActivitiesList.js
+++ src/components/ActivitiesList.js
@@ -7,13 +7,9 @@ const ActivitiesList = ({ items, onPressAdd, onPressDetails }) => {
   return (
     <View>
       <Title>Activities</Title>
-      <List.Section>
-        <List.Accordion title="Activities">
-          {
-            items.map(activity => <ActivitiesListItem key={activity['_id']} details={activity} onPressDetails={onPressDetails} />)
-          }
-        </List.Accordion>
-      </List.Section>
+      {
+        items.map(activity => <ActivitiesListItem key={activity['_id']} details={activity} onPressDetails={onPressDetails} />)
+      }
       <Button onPress={onPressAdd}>Add a new entry</Button>
     </View>
   );
