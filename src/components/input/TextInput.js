755e78d30a22ccb6a435516696910f4aa35681eb
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-21 14:59:42 +0200

    Added spacing

--- src/components/input/TextInput.js
+++ src/components/input/TextInput.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { HelperText, TextInput } from 'react-native-paper';
 
+import styles from '../../styles';
+
 const TextInputWithHelper = ({ value, placeholder, onChangeText, helperVisible, helperType, helperText, additionalHelperText }) => {
   return (
-    <View>
+    <View style={styles.layout.spacing}>
       <TextInput
         value={value || ''}
         placeholder={placeholder || ''}
@@ -12,15 +14,10 @@ const TextInputWithHelper = ({ value, placeholder, onChangeText, helperVisible, 
       />
       {
         additionalHelperText
-          ? (
-            <HelperText
-              type={'info'}
-              visible={true}
-            >
-              {additionalHelperText}
-            </HelperText>
-          )
-          : (<></>)
+          ? <HelperText visible={true}>
+            {additionalHelperText}
+          </HelperText>
+          : <View />
       }
       <HelperText
         type={helperType}
