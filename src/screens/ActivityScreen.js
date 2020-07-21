da0d567d4be3dfe0122f16ab85de7ea6747357e2
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-21 15:20:40 +0200

    Added converting dates and removed test entities

--- src/screens/ActivityScreen.js
+++ src/screens/ActivityScreen.js
@@ -3,12 +3,9 @@ import { View, Text, ScrollView, Alert } from 'react-native';
 import styles from '../styles';
 import ActivitiesList from '../components/ActivitiesList';
 import ActivityEditorModal from '../components/modals/ActivityEditorModal';
+import util from '../util';
 
-const defaultActivities = [
-  { _id: '111111111111111111111111', name: 'Swimming', category: 'swimming', startDate: 123, endDate: 456, distance: 5000 },
-  { _id: '111111111111111111111112', name: 'Cycling in mountains', category: 'cycling', startDate: 123, endDate: 456, distance: 45000 },
-  { _id: '111111111111111111111113', name: 'Football match', category: 'football', startDate: 123, endDate: 456 },
-];
+const defaultActivities = [];
 
 const ActivityScreen = () => {
   const [modifyModalVisible, setModifyModalVisible] = useState(false);
@@ -22,6 +19,10 @@ const ActivityScreen = () => {
     // add an activity
     const temp = [...activities];
     const details = {...activityDetails};
+    // convert dates to Unix
+    details.startDate = util.date.dateToUnix(activityDetails.startDate);
+    details.endDate = util.date.dateToUnix(activityDetails.endDate);
+    console.log(util.date.dateToUnix(activityDetails.startDate), util.date.dateToUnix(activityDetails.endDate), 'handleesijhodtfjdfgiuhndfijijhdf')
     details['_id'] = activityDetails.name; // TO-DO: change it
     temp.push(details);
     setActivities(temp);
@@ -30,6 +31,9 @@ const ActivityScreen = () => {
     // edit an activity
     const temp = [...activities];
     const details = activityDetails;
+    // convert dates to Unix
+    details.startDate = util.date.dateToUnix(activityDetails.startDate);
+    details.endDate = util.date.dateToUnix(activityDetails.endDate);
     details['_id'] = activityID; // TO-DO: change it
     temp[findActivityIndexByID(activityID)] = activityDetails;
     setActivities(temp);
