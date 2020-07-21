56fb76e35886f0855cfb3f88ef76c741b3f10d15
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-21 12:44:42 +0200

    Added edit handler

--- src/components/ActivitiesListItem.js
+++ src/components/ActivitiesListItem.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { View } from 'react-native';
 import { List, Card, Button, IconButton } from 'react-native-paper';
-import util from '../util';
 
-const ActivitiesListItem = ({ details, onDeleteActivity }) => {
+import util from '../util';
+import styles from '../styles';
+
+const ActivitiesListItem = ({ details, onPressEdit, onDeleteActivity }) => {
   const showDeleteAlert = () => {
     util.alert.showAlert(
       'Delete an activity',
@@ -18,12 +21,15 @@ const ActivitiesListItem = ({ details, onDeleteActivity }) => {
       <Card.Title title={details.name || "Default title"} />
       <Card.Content>
         <List.Accordion title={"Details"}>
-        <List.Item title={`Category: ${details.category || "uncategorized"}`} />
-        <List.Item title={`Start date: ${util.date.formatDateTime(details.startDate)}`} />
-        <List.Item title={`End date: ${util.date.formatDateTime(details.endDate)}`} />
-        { details.distance ? <List.Item title={`Distance: ${details.distance} meters`} /> : <></>}
+          <List.Item title={`Category: ${details.category || "uncategorized"}`} />
+          <List.Item title={`Start date: ${util.date.formatDateTime(details.startDate)}`} />
+          <List.Item title={`End date: ${util.date.formatDateTime(details.endDate)}`} />
+          {details.distance ? <List.Item title={`Distance: ${details.distance} meters`} /> : <></>}
         </List.Accordion>
-        <IconButton icon="trash-can-outline" onPress={showDeleteAlert} />
+        <View style={styles.layout.horizontal}>
+          <IconButton icon="pencil" onPress={onPressEdit} />
+          <IconButton icon="trash-can-outline" onPress={showDeleteAlert} />
+        </View>
       </Card.Content>
     </Card>
     /*
