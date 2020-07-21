207ef7a126bb1f19582fba6824ce473d34a157c3
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-21 11:44:20 +0200

    Added a new representation of activity details

--- src/components/ActivitiesListItem.js
+++ src/components/ActivitiesListItem.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { List } from 'react-native-paper';
+import { List, Card } from 'react-native-paper';
+import util from '../util';
 
 const ActivitiesListItem = ({ details, onPressDetails }) => {
+  console.log(details)
   return (
-    <>
+    <Card>
+      <Card.Title title={details.name || "Default title"} />
+      <Card.Content>
+        <List.Accordion title={"Details"}>
+        <List.Item title={`Category: ${details.category || "uncategorized"}`} />
+        <List.Item title={`Start date: ${util.date.formatDateTime(details.startDate)}`} />
+        <List.Item title={`End date: ${util.date.formatDateTime(details.endDate)}`} />
+        { details.distance ? <List.Item title={`Distance: ${details.distance} meters`} /> : <></>}
+        </List.Accordion>
+      </Card.Content>
+    </Card>
+    /*
       <List.Item
         title={details.name}
         onPress={() => onPressDetails(details['_id'])}
         right={props => <List.Icon {...props} icon="pencil" />}
       />
-    </>
-  )
+    */
+  );
 };
 export default ActivitiesListItem;
