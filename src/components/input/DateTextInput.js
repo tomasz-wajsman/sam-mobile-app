06877ff9bdfd9d6cd49b6cb0047dd94d0b3c8ed5
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-22 12:09:22 +0200

    Refactor
    Added date input, renamed text input

---
+++ src/components/input/DateTextInput.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { HelperText, TextInput as PaperTextInput } from 'react-native-paper';
+
+import PropTypes from 'prop-types';
+
+import styles from '../../styles';
+import util from '../../util';
+
+const DateTextInput = ({ defaultValue, placeholder, onChangeText, helperVisible, helperType, helperText, additionalHelperText }) => {
+  return (
+    <View style={styles.layout.spacing}>
+      <PaperTextInput
+        style={{ padding: 5, height: 24 }}
+        defaultValue={defaultValue}
+        placeholder={placeholder}
+        onChangeText={onChangeText}
+      />
+      {
+        additionalHelperText
+          ? <HelperText visible={true}>
+            {additionalHelperText}
+          </HelperText>
+          : <View />
+      }
+      <HelperText
+        type={helperType}
+        visible={helperText !== ''}
+      >
+        {helperText}
+      </HelperText>
+    </View>
+  );
+};
+
+DateTextInput.propTypes = {
+  defaultValue: PropTypes.string,
+  placeholder: PropTypes.string,
+  additionalHelperText: PropTypes.string
+};
+DateTextInput.defaultProps = {
+  defaultValue: util.date.unixToDate(0),
+  placeholder: '',
+  additionalHelperText: 'Date time format: YYYY-MM-DD HH:mm:SS'
+};
+
+export default DateTextInput;
---
+++ src/components/input/FormTextInput.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { HelperText, TextInput as PaperTextInput } from 'react-native-paper';
+
+import PropTypes from 'prop-types';
+import styles from '../../styles';
+
+const FormTextInput = ({ defaultValue, placeholder, onChangeText, helperVisible, helperType, helperText, additionalHelperText }) => {
+  return (
+    <View style={styles.layout.spacing}>
+      <PaperTextInput
+        style={{ padding: 5, height: 24 }}
+        defaultValue={defaultValue}
+        placeholder={placeholder}
+        onChangeText={onChangeText}
+      />
+      {
+        additionalHelperText
+          ? <HelperText visible={true}>
+            {additionalHelperText}
+          </HelperText>
+          : <View />
+      }
+      <HelperText
+        type={helperType}
+        visible={helperText !== ''}
+      >
+        {helperText}
+      </HelperText>
+    </View>
+  );
+};
+
+FormTextInput.propTypes = {
+  defaultValue: PropTypes.string,
+  placeholder: PropTypes.string,
+  additionalHelperText: PropTypes.string
+};
+FormTextInput.defaultProps = {
+  defaultValue: '',
+  placeholder: ''
+};
+
+export default FormTextInput;
